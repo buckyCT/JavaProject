aa9f2447c621d4dae6dfec5c985ce4582a74079f
buckyCT <byucedal123@gmail.com>
2021-06-21 22:44:00 +0300

    zZz

---
+++ src/replit/part3_string/VerifyContains.java
@@ -0,0 +1,20 @@
+package replit.part3_string;
+
+import java.util.Scanner;
+
+public class VerifyContains {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String word = scan.nextLine();
+        String sentence = scan.nextLine();
+        //WRITE YOUR CODE HERE
+
+        if(sentence.contains(word)){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
+        }
+
+
+    }
+}
